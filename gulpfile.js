bd3c3a495ff3643473e2a6f6dc60d8da3c53bfdb
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-04-05 09:55:19 -0400

    Added gulp processing for javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const include = require('gulp-include');
+const eslint = require('gulp-eslint');
+const isFixed = require('gulp-eslint-if-fixed');
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
+const uglify = require('gulp-uglify');
 const readme = require('gulp-readme-to-markdown');
 const merge = require('merge');
 
 
 let config = {
   src: {
+    jsPath: './src/js',
     scssPath: './src/scss',
   },
   dist: {
     cssPath: './static/css',
-    fontPath: './static/fonts'
+    fontPath: './static/fonts',
+    jsPath: './static/js',
   },
   packagesPath: './node_modules',
   sync: false,
@@ -59,6 +66,34 @@ function buildCSS(src, dest) {
     }))
     .pipe(rename({
       extname: '.min.css'
+    }))
+    .pipe(gulp.dest(dest));
+}
+
+// Base JS linting function (with eslint). Fixes problems in place.
+function lintJS(src, dest) {
+  dest = dest || config.src.jsPath;
+
+  return gulp.src(src)
+    .pipe(eslint({
+      fix: true
+    }))
+    .pipe(eslint.format())
+    .pipe(isFixed(dest));
+}
+
+function buildJS(src, dest) {
+  dest = dest || config.dist.jsPath;
+
+  return gulp.src(src)
+    .pipe(include({
+      includePaths: [config.packagesPath, config.src.jsPath]
+    }))
+    .on('error', console.log) // eslint-disable-line no console
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
     }))
     .pipe(gulp.dest(dest));
 }
@@ -117,6 +152,24 @@ gulp.task('css', gulp.series('scss-lint-plugin', 'scss-build-editor'));
 
 
 //
+// JS
+//
+
+// Run eslint on js files in src.jsPath
+gulp.task('es-lint-plugin', () => {
+  return lintJS([`${config.src.jsPath}/*.js`], config.src.jsPath);
+});
+
+// Concat and uglify js files through babel
+gulp.task('js-build-plugin', () => {
+  return buildJS(`${config.src.jsPath}/athena-editor-scripts.js`, config.dist.jsPath);
+});
+
+// All js-related tasks
+gulp.task('js', gulp.series('es-lint-plugin', 'js-build-plugin'));
+
+
+//
 // Documentation
 //
 
@@ -138,6 +191,7 @@ gulp.task('watch', (done) => {
   serverServe(done);
 
   gulp.watch(`${config.src.scssPath}/**/*.scss`, gulp.series('css', serverReload));
+  gulp.watch(`${config.src.jsPath}/**/*.js`, gulp.series('js', serverReload));
   gulp.watch('./**/*.php', gulp.series(serverReload));
 });
 
@@ -145,4 +199,4 @@ gulp.task('watch', (done) => {
 //
 // Default task
 //
-gulp.task('default', gulp.series('components', 'css', 'readme'));
+gulp.task('default', gulp.series('components', 'css', 'js', 'readme'));
